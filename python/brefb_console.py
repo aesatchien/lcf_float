4da88d6af2377d3b4b8fd0ed237717506e721ca0
aesatchien <cjh123@yahoo.com>
2022-11-21 21:14:12 -0800

    better console updates

--- python/brefb_console.py
+++ python/brefb_console.py
@@ -2,6 +2,7 @@
 # 20221030 CJH
 
 import pickle
+import curses
 import datetime
 import pandas as pd
 import time
@@ -12,28 +13,35 @@ import brEFB_CJH as brefb
 console_df = None  # just in case we somehow miss a definition
 dummy_df = None
 
+
 # debug - pull the dummy df from the disk - really not necessary after I dubugged it
-#with open('console.pkl', 'rb') as f:
-#    dummy_df = pickle.load(f)
+with open('dummy.pkl', 'rb') as f:
+    dummy_df = pickle.load(f)
 #for col in ['td dec', 'td 256', 'sp dec', 'sp 256', 'P gain', 'I gain', 'D gain', 'rev', 'td min', 'td max']:
-#    dummy_df[col]='NA'
+#  with   dummy_df[col]='NA'
 
 
 def get_updates():
-    if None in brefb.ipdict.values():  # not sure what else to do
-        # should I call the brefb.set_banks(verbose=True) again?
-        # brefb.set_banks(verbose=False)
-        return dummy_df
+    # if None in brefb.ipdict.values():  # not sure what else to do
+    #     # should I call the brefb.set_banks(verbose=True) again?
+    #     # brefb.set_banks(verbose=False)
+    #     return dummy_df
 
-    confa = brefb.get_conf_df('a')
-    state_a = brefb.current_state_df('a')
-    confb = brefb.get_conf_df('b')
-    state_b = brefb.current_state_df('b')
+    if list(brefb.ipdict.values())[0] == None:
+        temp_a = dummy_df.iloc[0:4]
+    else:
+        confa = brefb.get_conf_df('a')
+        state_a = brefb.current_state_df('a')
+        temp_a = pd.concat([state_a, confa], axis=1)
+        temp_a['axis'] = 'A' + temp_a['axis'].astype(str)
+    if list(brefb.ipdict.values())[1] == None:
+        temp_b = dummy_df.iloc[4:]
+    else:
+        confb = brefb.get_conf_df('b')
+        state_b = brefb.current_state_df('b')
+        temp_b = pd.concat([state_b, confb], axis=1)
+        temp_b['axis'] = 'B' + temp_b['axis'].astype(str)
 
-    temp_a = pd.concat([state_a, confa], axis=1)
-    temp_b = pd.concat([state_b, confb], axis=1)
-    temp_a['axis'] = 'A' + temp_a['axis'].astype(str)
-    temp_b['axis'] = 'B' + temp_b['axis'].astype(str)
     console_df = pd.concat([temp_a, temp_b])
     console_df.columns = ['axis', 'td dec', 'td 256', 'sp dec', 'sp 256', 'P gain', 'I gain', 'D gain', 'rev', 'td min',
                           'td max']
@@ -56,18 +64,18 @@ if __name__ == '__main__':
                   f'\n1) Are we connected to animation network?' +
                   f'\n2) Are the BREFBs powered up?\n' +
                   f'exiting!')
-            exit(1)
+            # exit(1)
     except Exception as e:
         print(f'Unable to communicate with BREFB!' +
               f'\nAre we connected to animation network?')
 
     while True:
-        os.system('cls')
+        console_df = get_updates()
         now = datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S')
         msg = f'*** BREFB POLLING DATA FOR {now} ***  (press Ctrl-c to exit)'
         offset = (80 - len(msg)) // 2
+        os.system('clear')
         print(f'{" " * offset}{msg}')
         print(f'\nIP dict: {brefb.ipdict}\n')
-        console_df = get_updates()
-        print(console_df)
+        print(console_df, flush=True)
         time.sleep(1)
---
+++ python/test_curses.py
@@ -0,0 +1,111 @@
+# script to display all the LCFToR float BREFB values for tuning mechanisms
+# NOTE: for curses to work on windows you have to call with winpty python script.py
+# 20221030 CJH
+
+import pickle
+import curses
+import datetime
+import pandas as pd
+import time
+import os
+import signal
+import brEFB_CJH as brefb
+
+console_df = None  # just in case we somehow miss a definition
+dummy_df = None
+
+# debug - pull the dummy df from the disk - could explicitly define it in code, i guess
+with open('console.pkl', 'rb') as f:
+    dummy_df = pickle.load(f)
+#for col in ['td dec', 'td 256', 'sp dec', 'sp 256', 'P gain', 'I gain', 'D gain', 'rev', 'td min', 'td max']:
+#  with   dummy_df[col]='NA'
+
+
+def get_updates():
+
+    if list(brefb.ipdict.values())[0] == None:
+        temp_a = dummy_df.iloc[0:4]  # just show blanks
+    else:
+        confa = brefb.get_conf_df('a')
+        state_a = brefb.current_state_df('a')
+        temp_a = pd.concat([state_a, confa], axis=1)
+        temp_a['axis'] = 'A' + temp_a['axis'].astype(str)
+    if list(brefb.ipdict.values())[1] == None:
+        temp_b = dummy_df.iloc[4:]  # just show blanks
+    else:
+        confb = brefb.get_conf_df('b')
+        state_b = brefb.current_state_df('b')
+        temp_b = pd.concat([state_b, confb], axis=1)
+        temp_b['axis'] = 'B' + temp_b['axis'].astype(str)
+
+    console_df = pd.concat([temp_a, temp_b])
+    console_df.columns = ['axis', 'td dec', 'td 256', 'sp dec', 'sp 256', 'P gain', 'I gain', 'D gain', 'rev', 'td min',
+                          'td max']
+    return console_df
+
+if __name__ == '__main__':
+    screen = curses.initscr()
+    warning_line = 20
+    screen.clear()
+    curses.cbreak()
+    screen.nodelay(True) # take character input
+
+    try:
+        screen.addstr(0, 0, '- Program to monitor the transducers, SPs and PIDs of the BrEFBs - ')
+        screen.addstr(1, 0, 'Connecting to brefbs...')
+        screen.refresh()
+        brefb.set_banks(verbose=True)
+        if None in list(brefb.ipdict.values()):
+            screen.addstr(warning_line, 0, f'Unable to communicate with both BREFB a and b!' +
+                  f'\n1) Are we connected to animation network?' +
+                  f'\n2) Are the BREFBs powered up?\n' +
+                  f'Consider exiting!')
+            screen.refresh()
+            curses.napms(2000)
+            screen.clear()
+            # exit(1)
+    except Exception as e:
+        screen.addstr(warning_line, 0, f'Unable to communicate with BREFB!' +
+              f'\nAre we connected to animation network?')
+
+    # add a few colors to make life easier
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+    while True:
+        console_df = get_updates()
+        now = datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S')
+        msg = f'*** BREFB POLLING DATA FOR {now} ***'
+        offset = (80 - len(msg)) // 2
+
+        # Update the buffer, adding text at different locations
+        screen.addstr(0, 0, f'{" " * offset}{msg}')
+        screen.addstr(2, 0, f'IP dict: {brefb.ipdict}\n')  # Python 3 required for unicode
+        s = console_df.to_string().split('\n')
+        screen.addstr(4, 0, s[0])
+        screen.addstr(5, 0, '\n'.join(s[1:5]), curses.color_pair(2))
+        screen.addstr(9, 0, '\n'.join(s[5:]), curses.color_pair(3))
+
+        quit_msg = f'press q to quit: '
+        screen.addstr(warning_line, len(quit_msg), ' ')  # clear non-quittng characters
+        screen.addstr(warning_line, 0, quit_msg, curses.color_pair(1))
+
+        curses.curs_set(True) # lose the blinking cursor
+
+        c = screen.getch()  # break if we hit the q key, note nodelay() is on
+        if c == ord('q'):
+            screen.addstr(warning_line, 0, f'Exit key pressed ... Goodbye!')
+            screen.refresh()
+            curses.napms(2500)
+            break
+        # Changes go in to the screen buffer and only get displayed after calling `refresh()` to update
+        screen.refresh()
+        curses.napms(1000)
+
+    # put the console back to normal
+    curses.nocbreak()
+    screen.keypad(False)
+    curses.echo()
+    curses.endwin()
